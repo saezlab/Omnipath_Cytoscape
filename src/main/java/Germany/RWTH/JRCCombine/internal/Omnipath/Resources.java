89fb2baa37314407f910ce69fd0b2f6d8354c27b
Francesco Ceccarelli <cecca46@live.it>
2020-02-18 11:58:39 +0000

    change in the references format

--- src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/Resources.java
+++ src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/Resources.java
@@ -4,11 +4,12 @@ import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.stream.*;
 
 import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
@@ -59,27 +60,26 @@ public class Resources extends AbstractCyAction {
 			
 			String uniqueName = network.getRow(s).get("shared name", String.class);
 			String references = network.getRow(s).get("references", String.class);
-			List<String> uniqueReferences = Arrays.asList(
-					references.split(";")
-				).stream().map(
-					(String ref) -> {
-						String refs[] = ref.split(":");
-						return refs.length == 2 ? refs[1] : refs[0];
-					}
-				).collect(Collectors.toList()
-			);
-			Set<String> temp = new LinkedHashSet<String>(uniqueReferences);
+			List<String> uniqueReferences = Arrays.asList(references.split(";"));
+			ArrayList<String> p = new ArrayList<String>();
+			for (String x : uniqueReferences) {
+				String refs[] = x.split(":");
+				if (refs.length == 2) {
+					p.add(refs[1]);
+				}else 
+					p.add(refs[0]);
+			}
+			Set<String> temp = new LinkedHashSet<String>(p);
 			String[] result = temp.toArray( new String[temp.size()] );
 
 			String links="";
-			for (String p : result) {
-				String tmp1 = "https://www.ncbi.nlm.nih.gov/pubmed/"+p;
+			for (String x : result) {
+				String tmp1 = "https://www.ncbi.nlm.nih.gov/pubmed/"+x;
 				String tmp2 = "<a href=\""+tmp1+"\">";
 				String tmp3 = tmp2 + tmp1 + "</a><br /><br />";
 				links = links + tmp3;
 				
 			}
-			
 			
 			 JEditorPane ep =new JEditorPane("text/html","<hml><body>Edge between "+ source + " and " + target + "<br /><br />"
 					 + "The following PubMed identifiers support this interaction:<br /><br />" 
