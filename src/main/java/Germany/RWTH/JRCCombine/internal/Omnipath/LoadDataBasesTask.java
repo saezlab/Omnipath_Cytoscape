131881d3f78ec93426d2be3c85b65cdde24842e9
Nicolas Palacio <nicolaspalacio91@gmail.com>
2026-03-03 11:27:59 +0100

    Fixed some legacy URLs

--- src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/LoadDataBasesTask.java
+++ src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/LoadDataBasesTask.java
@@ -18,36 +18,36 @@ import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
 
 public class LoadDataBasesTask extends AbstractTask {
-	
-	
+
+
 	private CySwingAppAdapter adapter;
 	private String database;
 	private String organism;
 	private CyApplicationManager applicationManager;
 	private DualListBox dual;
-	
+
 	public LoadDataBasesTask(CySwingAppAdapter adapter,
 			String database, String  organism, CyApplicationManager applicationManager, DualListBox dual) {
-		
+
 		this.adapter = adapter;
 		this.organism = organism;
 		this.database = database;
-		this.applicationManager = applicationManager; 
+		this.applicationManager = applicationManager;
 		this.dual = dual;
-		
+
 	}
 
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 		taskMonitor.setTitle("Omnipath -- Querying list of Databases from the server...");
 		dual.clearDestinationListModel();
 		dual.clearSourceListModel();
 		if (database.equals("Signaling networks") || database.equals("TF-target interactions") || database.equals("miRNA-mRNA")) {
-			
-			
-			URL oracle = new URL("https://omnipathdb.org/databases/interactions/");
+
+
+			URL oracle = new URL("https://omnipathdb.org/interactions/");
 		    BufferedReader in = new BufferedReader(
 		    new InputStreamReader(oracle.openStream()));
 		    in.readLine();
@@ -63,29 +63,29 @@ public class LoadDataBasesTask extends AbstractTask {
 		    	multiMap.put(split[0], values);
 		    }
 		    in.close();
-		    
+
 		    String[] array;
 		    if (database.equals("Signaling networks")){
-		    	
+
 		    	array = new String[multiMap.get("post_translational").size()];
 		    	array = multiMap.get("post_translational").toArray(array);
-		    	
-		    	
+
+
 		    }else if (database.equals("TF-target interactions")) {
 		    	array = new String[multiMap.get("transcriptional").size()];
 		    	array = multiMap.get("transcriptional").toArray(array);
-		    	
+
 		    }else {
-		    	
+
 		    	array = new String[multiMap.get("post_transcriptional").size()];
 		    	array = multiMap.get("post_transcriptional").toArray(array);
 		    }
 			dual.addSourceElements(array);
 
-			
+
 		}else {
-			
-			URL oracle = new URL("https://omnipathdb.org/databases/ptms/");
+
+			URL oracle = new URL("https://omnipathdb.org/enzsub/");
 		    BufferedReader in = new BufferedReader(
 		    new InputStreamReader(oracle.openStream()));
 		    in.readLine();
@@ -100,17 +100,17 @@ public class LoadDataBasesTask extends AbstractTask {
 		    	ArrayList<String> values = new ArrayList(Arrays.asList(databases));
 		    	multiMap.put(split[0], values);
 		    }
-		        
+
 		    in.close();
 		    String[] array;
 		    if (database.equals("Enzyme-substrate interactions")){
 		    	array = new String[multiMap.get("*").size()];
 		    	array = multiMap.get("*").toArray(array);
 		    	dual.addSourceElements(array);
-		    }    
-		    
+		    }
+
 		}
-		
+
 	}
-	
+
 }
--- src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/LoadDataOptions.java
+++ src/main/java/Germany/RWTH/JRCCombine/internal/Omnipath/LoadDataOptions.java
@@ -36,14 +36,14 @@ public class LoadDataOptions {
 	private boolean isSigned;
 
 	public LoadDataOptions() {}
-	
-	
-	public void getDatabse(String database, String organism, CySwingAppAdapter adapter, 
+
+
+	public void getDatabse(String database, String organism, CySwingAppAdapter adapter,
 						   CyApplicationManager applicationManager,
 						   Object[] selected, ArrayList<String> selectedConfidence, boolean isTF,
-						   boolean isDirected, boolean isSigned) 
+						   boolean isDirected, boolean isSigned)
 						   throws IOException, InterruptedException {
-		
+
 		this.adapter = adapter;
 		//this.pathways = pathways;
 		//this.resources = resources;
@@ -55,113 +55,113 @@ public class LoadDataOptions {
 		this.isTF = isTF;
 		this.isDirected = isDirected;
 		this.isSigned = isSigned;
-		
-		
-		//start query formulation for interactions or ptms 
+
+
+		//start query formulation for interactions or ptms
 		// interactions query
 		if (database.equals("Signaling networks") || database.equals("miRNA-mRNA") || database.equals("TF-target interactions")) {
-			
+
 			query = "https://omnipathdb.org/interactions?fields=sources&fields=references&genesymbols=1";
 			//add extra text if it is TF query
 			if (database.equals("TF-target interactions")) {
-				
+
 				query= query +"&types=transcriptional";
-				
+
 			}
 			//add extra text if it is miRNA query
 			if (database.equals("miRNA-mRNA")) {
-				
+
 				query = query +"&datasets=mirnatarget";
-				
+
 			}
-		
+
 		}
 		//ptms query
 		else {
-			
-			query = "https://omnipathdb.org/ptms/?fields=sources&fields=references&genesymbols=1";
-			
+
+			query = "https://omnipathdb.org/enzsub?fields=sources&fields=references&genesymbols=1";
+
 		}
-		
+
 		//start query formulation for organism type
 		if (organism.equals("Mouse")) {
-			
+
 			query = query + "&organisms=10090";
-			
+
 		}
 		else if (organism.equals("Rat")) {
-			
+
 			query = query + "&organisms=10116";
-			
+
 		}
 		// human organism by default
-		// if human then do not add extra fields to the query 
+		// if human then do not add extra fields to the query
 		else {}
-		
+
 		// add database selected
 		query = query + "&databases=";
 		for (int i = 0; i< selected.length; i++) {
-			
+
 			if (i == 0)
 				query = query +selected[i].toString();
-			else 
+			else
 				query = query + ","+selected[i].toString();
-			
+
 		}
-		
-		
-		// if TF interactions, add the confidence levels 
+
+
+		// if TF interactions, add the confidence levels
 		if (isTF) {
 			query = query + "&tfregulons_levels=";
 			for (int i = 0; i< selectedConfidence.size(); i++) {
-				
+
 				if (i == 0)
 					query = query +selectedConfidence.get(i).toString();
-				else 
+				else
 					query = query + ","+selectedConfidence.get(i).toString();
-				
+
 			}
 		}
-		// if directed is selected 
+		// if directed is selected
 		if(isDirected) {
-			
+
 			// add to query
 			query = query + "&directed=yes";
 		}
-		
-		// if signed is selected 
+
+		// if signed is selected
 		if (isSigned) {
-			
+
 			// add to query
 			query = query + "&signed=1";
 		}
-		
-		
+
+
 		// remove all spaces from the query
-		// in case there are any 
+		// in case there are any
 		query = query.replaceAll("\\s+","");
-			
+
 		//JOptionPane.showMessageDialog(null, query);
-		
-		//check for wrong user selection 
+
+		//check for wrong user selection
 		if ((database.equals("miRNA-mRNA")) && (organism.equals("Mouse") || organism.equals("Rat"))) {
-			
+
 			JOptionPane.showMessageDialog(null, "miRNA are provided only for Humans. Please change selected organism to human or select a new database.",
             		"Error Message", JOptionPane.ERROR_MESSAGE);
 		}
-		
-		// else create task to execute the query 
+
+		// else create task to execute the query
 		// and plot the network in Cytoscape
 		else {
-			QuerySentTaskObserver taskObserver = new QuerySentTaskObserver();  
-			SendQueryTaskFactory queryTaskFactory = new SendQueryTaskFactory(query, adapter, database, organism, applicationManager);	
+			QuerySentTaskObserver taskObserver = new QuerySentTaskObserver();
+			SendQueryTaskFactory queryTaskFactory = new SendQueryTaskFactory(query, adapter, database, organism, applicationManager);
 			adapter.getTaskManager().execute(queryTaskFactory.createTaskIterator(), taskObserver);
-			
-			
+
+
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 }
